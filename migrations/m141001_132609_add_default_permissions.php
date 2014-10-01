f6f563334b32077882d40349d3cab5c4d2dc2881
Fabio Maggio <fabio@infoweb.be>
2014-10-01 15:36:31 +0200

    Update migrations

---
+++ migrations/m141001_132609_add_default_permissions.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141001_132609_add_default_permissions extends Migration
+{
+    public function up()
+    {
+        // Create the auth items
+        $this->insert('{{%auth_item}}', [
+            'name'          => 'showContentModule',
+            'type'          => 2,
+            'description'   => 'Show content module icon in main-menu',
+            'created_at'    => time(),
+            'updated_at'    => time()
+        ]);
+        
+        $this->insert('{{%auth_item}}', [
+            'name'          => 'showPagesModule',
+            'type'          => 2,
+            'description'   => 'Show pages module in main-menu',
+            'created_at'    => time(),
+            'updated_at'    => time()
+        ]);
+        
+        // Create the auth item relation
+        $this->insert('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showContentModule'
+        ]);
+        
+        $this->insert('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showPagesModule'
+        ]);
+    }
+
+    public function down()
+    {
+        // Delete the auth item relation
+        $this->delete('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showContentModule'
+        ]);
+        
+        $this->delete('{{%auth_item_child}}', [
+            'parent'        => 'Superadmin',
+            'child'         => 'showPagesModule'
+        ]);
+        
+        // Delete the auth items
+        $this->delete('{{%auth_item}}', [
+            'name'          => 'showContentModule',
+            'type'          => 2,
+        ]);
+        
+        $this->delete('{{%auth_item}}', [
+            'name'          => 'showPagesModule',
+            'type'          => 2,
+        ]);
+    }
+}
