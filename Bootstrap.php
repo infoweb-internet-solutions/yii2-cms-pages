6eba1774ac30082fa7ffc566c14b594b9aa6ece9
Fabio Maggio <fabio@infoweb.be>
2015-05-28 09:19:49 +0200

    Implement bootstrap interface
    Moved all the initialisation code to the bootstrap interface.

---
+++ Bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+namespace infoweb\pages;
+
+use Yii;
+use yii\base\BootstrapInterface;
+use yii\base\Event;
+use yii\db\ActiveRecord;
+use infoweb\pages\models\Page;
+
+class Bootstrap implements BootstrapInterface
+{
+    /** @inheritdoc */
+    public function bootstrap($app)
+    {
+        // Set eventhandlers
+        $this->setEventHandlers();
+        
+        // If a page slider is enabled in the module, the sliders module has to
+        // be activated
+        $module = $app->getModule('pages');
+        
+        if ($module->enableSliders && !$app->hasModule('sliders')) {
+            // Disable sliders in the module
+            $module->enableSliders = false;        
+        }
+    }
+    
+    protected function setEventHandlers()
+    {
+        // Set eventhandlers for the 'Page' model
+        Event::on(Page::className(), ActiveRecord::EVENT_BEFORE_DELETE, function ($event) {
+            
+            // Check if the page is the homepage
+            if ($event->sender->homepage == 1)
+                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'The page can not be deleted because it is the homepage'));
+            
+            // Check if the page is not used in a menu
+            if ($event->sender->isUsedInMenu())
+                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'The page can not be deleted because it is used in a menu'));
+            
+            // Delete the attached entities
+            if (!$event->sender->deleteAttachedEntities())
+                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'Error while deleting the page'));
+        });    
+    }
+}
--- Module.php
+++ Module.php
@@ -3,37 +3,19 @@
 namespace infoweb\pages;
 
 use Yii;
-use yii\base\Event;
-use yii\db\ActiveRecord;
-use infoweb\pages\models\Page;
 
 class Module extends \yii\base\Module
 {
+    /**
+     * Enable link between a page an a slider
+     * @var boolean
+     */
+    public $enableSliders = false;
+    
     public function init()
     {
         parent::init();
 
-        Yii::configure($this, require(__DIR__ . '/config.php'));
-        
-        $this->setEventHandlers();
-    }
-    
-    public function setEventHandlers()
-    {
-        // Set eventhandlers for the 'Page' model
-        Event::on(Page::className(), ActiveRecord::EVENT_BEFORE_DELETE, function ($event) {
-            
-            // Check if the page is the homepage
-            if ($event->sender->homepage == 1)
-                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'The page can not be deleted because it is the homepage'));
-            
-            // Check if the page is not used in a menu
-            if ($event->sender->isUsedInMenu())
-                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'The page can not be deleted because it is used in a menu'));
-            
-            // Delete the attached entities
-            if (!$event->sender->deleteAttachedEntities())
-                throw new \yii\base\Exception(Yii::t('infoweb/pages', 'Error while deleting the page'));
-        });    
+        Yii::configure($this, require(__DIR__ . '/config.php')); 
     }
 }
