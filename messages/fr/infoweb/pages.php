426a65d6e7431831ee816f15d696f74a42067264
Fabio Maggio <fabio@infoweb.be>
2015-06-04 11:03:18 +0200

    Update translations

---
+++ messages/fr/infoweb/pages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Error while deleting the page' => '',
+    'Homepage' => '',
+    'Large image' => '',
+    'Page' => '',
+    'Page ID' => '',
+    'Pages' => '',
+    'Set as homepage' => '',
+    'Small image' => '',
+    'Snippet' => '',
+    'The page can not be deleted because it is the homepage' => '',
+    'The page can not be deleted because it is used in a menu' => '',
+];
